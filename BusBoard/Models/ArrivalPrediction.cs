d9d81b225d6f787afc64fcaf73303aa5fa271cdd
meganrdp <100476117+meganrdp@users.noreply.github.com>
2023-10-20 14:10:17 +0100

    added bus number, destination and time in minutes

--- BusBoard/Models/ArrivalPrediction.cs
+++ BusBoard/Models/ArrivalPrediction.cs
@@ -3,4 +3,8 @@ namespace BusBoard.Models;
 public class ArrivalPrediction
 {
     public int TimeToStation { get; set; }
+    public string DestinationName { get; set; }
+    public string LineName { get; set; }
+
 }
+
